c97f1c13f9601eff3968b3abe1001927d988ef3e
Mathis BRIEND <mathis.briend22@gmail.com>
2024-03-21 19:12:18 +0100

    router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Teams from './components/Teams/Teams';
 import Games from './components/Games/Games';
-import {BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {Route, Routes } from "react-router-dom";
 import HomePage from './components/HomePage/HomePage';
 
 const App = () => {
@@ -10,14 +10,12 @@ const App = () => {
 
     <div className="App">
 
-      <Router>
         <Routes>
           <Route path='' element={<HomePage></HomePage>}></Route>
           <Route path='teams' element={<Teams></Teams>}></Route>
           <Route path='teams/games/:teamId' element={<Games title="Team Games" key={Math.random()}></Games>}></Route>
           <Route path='games' element={<Games title="Today Games" key={Math.random()}></Games>}></Route>
         </Routes>
-      </Router>
 
     </div>
 
